5e4ca76f0cb82bb18cc8605ea5950997fd94b429
Michael Stonis <michael.stonis@eightbot.com>
2023-11-09 12:38:27 -0600

    feat: Update Visual Studio version and add platform-specific classes
    - Updated the Visual Studio version to 17.8.34227.203
    - Added platform-specific classes for Android, iOS, Mac Catalyst, Tizen, and Windows
    - Created a new class `StripePaymentHandler` with a method `ShowPaymentSheet()` that is included in all platforms

---
+++ Stripe.MAUI/StripePaymentHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Stripe.MAUI
+{
+    // All the code in this file is included in all platforms.
+    public class StripePaymentHandler
+    {
+        public void ShowPaymentSheet()
+        {
+
+        }
+    }
+}
