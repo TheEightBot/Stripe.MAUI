b2be570ad6976ccb68a47030e813f5f9ac13c249
Michael Stonis <michael.stonis@eightbot.com>
2023-12-05 12:52:08 -0600

    ios setup
    You will need to unzip the zip in the folder to have sharpie dig through it all.

---
+++ Stripe.MAUI.iOS/objectivesharpie.sh
@@ -0,0 +1,2 @@
+﻿#!/usr/bin/env bash
+sharpie bind --namespace=Stripe --sdk=iphoneos16.1 -scope ../lib/iOS/Stripe -framework ../lib/iOS/Stripe/StripePaymentSheet.xcframework/ios-arm64/StripePaymentSheet.framework
